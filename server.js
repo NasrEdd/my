2cc54f7c8f725b1c8e792371bb6d175f9b1ec404
NasrEdd <107544968+NasrEdd@users.noreply.github.com>
2022-06-27 16:01:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/nasrnetwaciila'));
+app.use(express.static(__dirname + '/dist/pulse-registrariat'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname +
         '/dist/pulse-registrariat/index.html'));
