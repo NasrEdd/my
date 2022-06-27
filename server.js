3c28396e63293f6027e2beab795bfae69c63e581
NasrEdd <107544968+NasrEdd@users.noreply.github.com>
2022-06-27 15:54:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 app.use(express.static(__dirname + '/dist/nasrnetwaciila'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname +
-        '/src/index.html'));
+        '/dist/pulse-registrariat/index.html'));
 });
 app.listen(process.env.PORT || 8080);
